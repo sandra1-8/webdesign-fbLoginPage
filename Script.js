90fe3b7f19189f2ae9032fcf05cd43ed4eecc9c5
Sandra <19cs189@mgits.ac.in>
2022-02-13 11:55:21 +0530

    Design of fb login page as part of WebDesign Challenge/Brototype

---
+++ Script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:4,
+                maxlength:6
+            },
+            sname:{
+                required:true,
+                minlength:4
+            },
+            emailAddress:{
+                required:true,
+                email:true
+            },
+            password:{
+                minlength:6
+            },
+            day:{
+                required:true
+            },
+            month:{
+                required:true
+            },
+            year:{
+                required:true
+            }
+            gender:{
+                required:true
+            },
+            messages:{
+                fname:{
+                    required:"enter first name",
+                    minlength:"enter atleast 4"
+                }
+            }
+
+        }
+    })
+})
